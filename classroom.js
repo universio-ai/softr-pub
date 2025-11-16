7c99724aa3eb27186252d26ede64d287db6a372e
universio-ai <info@universio.ai>
2025-11-16 17:30:57 -0500

    Update classroom.js

--- classroom.js
+++ classroom.js
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <div id="output"></div>
-
-    <script>
-      document.getElementById("output").innerText =
-        "Hello, world. This is a test prior to migrating Classroom code from HTML to Javascript.";
-    </script>
-  </body>
-</html>
+document.body.innerText = "Hello, world. This is a test prior to migrating Classroom code from HTML to Javascript.";
