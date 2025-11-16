160b1593491ecdd4dbe7c9c81e6284e1e2d82f45
universio-ai <info@universio.ai>
2025-11-16 15:04:52 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Softr external JS file reached.");
+
+window.addEventListener("load", () => {
+  console.log("Softr: attempting external script load...");
+  console.log("External script loaded via GitHub Pages or JSDelivr!");
+});
