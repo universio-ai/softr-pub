c0f2ce087e666bb9995a7ddf4f0095926850e65a
universio-ai <info@universio.ai>
2025-11-16 17:59:28 -0500

    Add emoji to console message

--- classroom-app.js
+++ classroom-app.js
@@ -2,5 +2,5 @@ console.log("Hello, world. This is a test prior to migrating Classroom code from
 
 window.addEventListener("load", () => {
   console.log("Softr: attempting external script load...");
-  console.log("External script loaded via GitHub Pages or JSDelivr!");
+  console.log("External script loaded via GitHub Pages or JSDelivr! 🚀");
 });
