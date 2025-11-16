4bed8c57dabbb53887555d09254bcae5615b7429
universio-ai <info@universio.ai>
2025-11-16 18:10:52 -0500

    Update greeting message

--- classroom-app.js
+++ classroom-app.js
@@ -1,6 +1,6 @@
-console.log("Hello, world. This is a test prior to migrating Classroom code from HTML to Javascript.");
+console.log("Hey there, world. This is a test prior to migrating Classroom code from HTML to Javascript. 😊");
 
 window.addEventListener("load", () => {
   console.log("Softr: attempting external script load...");
-  console.log("External script loaded via GitHub Pages or JSDelivr!");
+  console.log("External script loaded via GitHub Pages or JSDelivr! 🚀");
 });
