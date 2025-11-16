9d242092f58433376245ee61141fe26ee303271b
universio-ai <info@universio.ai>
2025-11-16 17:38:10 -0500

    Delete classroom.js

--- classroom.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello, world. This is a test prior to migrating Classroom code from HTML to Javascript.");
-
-window.addEventListener("load", () => { 
-  console.log("Softr: attempting external script load...");
-  console.log("External script loaded via GitHub Pages or JSDelivr!");
-});
