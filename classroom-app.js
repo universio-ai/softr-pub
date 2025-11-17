220b23b3b4bbdf11b850f93b1072969e4e9f0865
universio-ai <info@universio.ai>
2025-11-16 20:12:24 -0500

    Update classroom-app.js

--- classroom-app.js
+++ classroom-app.js
@@ -2427,7 +2427,7 @@ injectStyles(`
             [
                 el("span", {
                     html: `
-  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
        viewBox="0 0 24 24" fill="none" stroke="#ffffff"
        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
        style="display:block;shape-rendering:geometricPrecision">
